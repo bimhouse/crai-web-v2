564c1bf404da2c6d687a076e3d414da7015f2d0c
gregoiredechaine <gregdechaine@me.com>
2020-08-17 23:17:08 -0400

    Add in fragment for routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, ExtraOptions } from '@angular/router';
 
 //CRAi Components
 import { CraiNavComponent } from './crai-nav/crai-nav.component';
@@ -9,6 +9,12 @@ import { CraiHomeComponent } from './crai-home/crai-home.component';
 import { CraiWorkComponent } from './crai-work/crai-work.component';
 import { CraiWorkDetailComponent } from './crai-work/crai-work-detail.component';
 import { CraiEmailsigComponent } from './crai-emailsig/crai-emailsig.component';
+
+const routerOptions: ExtraOptions = {
+  scrollPositionRestoration: 'enabled',
+  anchorScrolling: 'enabled',
+  scrollOffset: [0, 60],
+};
 
 const routes: Routes = [
   { path: '', component: CraiHomeComponent, pathMatch: 'full' },
@@ -20,7 +26,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, routerOptions)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
